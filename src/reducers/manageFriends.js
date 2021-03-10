9507c03bf48448971663bca0ea8b05e33a9f1a70
zaynegoering <zaynegoering@gmail.com>
2021-03-09 17:23:27 -0800

    commit tree 3a47e09c712f6146dd6284489368a93a64b55774

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,24 @@
-export function manageFriends(state, action){
+export function manageFriends(state = {
+    friends: [],
+}, action){
+
+    switch (action.type) {
+        case "ADD-FRIEND":
+            return ({
+                ...state,
+                friends: [...state.friends, action.friend]
+            })
+
+        case "REMOVE_FRIEND":
+            const indexRemove = state.friends.findIndex(friend => friend.id === action.id)
+            return ({
+                ...state,
+                friends: [...state.friends.slice(0, indexRemove), ...state.friends.slice(indexRemove + 1)]
+            })
+
+        default:
+            return state
+    }
 }
+
+
